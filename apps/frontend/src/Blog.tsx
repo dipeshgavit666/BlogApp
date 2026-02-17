fe3b0f6e287f98eb17efcd24e35440fe385738fb
dipeshgavit666 <dipeshgavit333@gmail.com>
2026-02-17 12:18:28 +0530

    fetch posts from api

--- apps/frontend/src/Blog.tsx
+++ apps/frontend/src/Blog.tsx
@@ -1,45 +1,44 @@
-import './App.css';
-import { CreatePost } from './components/CreatePost';
-import { PostList } from './components/PostList';
+import "./App.css";
+import { CreatePost } from "./components/CreatePost";
+import { PostList } from "./components/PostList";
 // import { Post } from './components/Post';
-import { PostFilter } from './components/PostFilter';
-import { PostSorting } from './components/PostSorting';
+import { PostFilter } from "./components/PostFilter";
+import { PostSorting } from "./components/PostSorting";
 
 export function Blog() {
-    const posts = [
-        {
-            _id: '1',
-            title: 'Full-Stack React Projects',
-            contents: "Let's become full-stack developers!",
-            author: 'Daniel Bugl',
-        },
-        {
-            _id: '2',
-            title: 'Full-Stack React Projects',
-            contents: "Let's become full-stack developers!",
-            author: 'Daniel Bugl',
-        },
-        {
-            _id: '3',
-            title: 'Full-Stack React Projects',
-            contents: "Let's become full-stack developers!",
-            author: 'Daniel Bugl',
-        },
-    ];
-    return (
-        <>
-            <div>
-                <CreatePost />
-                <br />
-                <hr />
-                Filter By:
-                <PostFilter field={'author'} />
-                <br />
-                <PostSorting fields={['createdAt', 'updatedAt']} />
-                <hr />
-                <PostList posts={posts} />
-            </div>
-        </>
-    );
+  const posts = [
+    {
+      _id: "1",
+      title: "Full-Stack React Projects",
+      contents: "Let's become full-stack developers!",
+      author: "Daniel Bugl",
+    },
+    {
+      _id: "2",
+      title: "Full-Stack React Projects",
+      contents: "Let's become full-stack developers!",
+      author: "Daniel Bugl",
+    },
+    {
+      _id: "3",
+      title: "Full-Stack React Projects",
+      contents: "Let's become full-stack developers!",
+      author: "Daniel Bugl",
+    },
+  ];
+  return (
+    <>
+      <div>
+        <CreatePost />
+        <br />
+        <hr />
+        Filter By:
+        <PostFilter field={"author"} />
+        <br />
+        <PostSorting fields={["createdAt", "updatedAt"]} />
+        <hr />
+        <PostList posts={posts} />
+      </div>
+    </>
+  );
 }
-
---
+++ apps/frontend/src/api/post.ts
@@ -0,0 +1,7 @@
+export const getPost = async (queryParams: Record<string, string>) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
+      new URLSearchParams(queryParams),
+  );
+  return await res.json();
+};
